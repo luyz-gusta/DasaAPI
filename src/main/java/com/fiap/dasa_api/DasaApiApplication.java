f3aaced66c15a7a909a66784e9edab3dd16b3c5a
luyz-gusta <luizgustavo.sp2020@gmail.com>
2025-05-21 17:46:40 -0300

    Iniciando projeto

---
+++ src/main/java/com/fiap/dasa_api/DasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.dasa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/dasa_api/DasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.dasa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
